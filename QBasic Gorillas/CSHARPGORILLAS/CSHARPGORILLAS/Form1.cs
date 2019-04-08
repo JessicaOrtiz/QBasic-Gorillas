560ddf605c4b5f2570156fe0bd9cd61db764b24e
Fernando Martinez <43658388+zackelF@users.noreply.github.com>
2019-04-08 15:08:57 -0500

    CreacionEscenarios
    tengo un pequeño detalle con coordenadas Xd

--- QBasic Gorillas/CSHARPGORILLAS/CSHARPGORILLAS/Form1.cs
+++ QBasic Gorillas/CSHARPGORILLAS/CSHARPGORILLAS/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CSHARPGORILLAS
 {
@@ -15,6 +16,7 @@ namespace CSHARPGORILLAS
         public Form1()
         {
             InitializeComponent();
+            CreaEscenario();
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -22,8 +24,25 @@ namespace CSHARPGORILLAS
 
         }
 
-        private void pictureBox3_Click(object sender, EventArgs e)
+        private void CreaEscenario()
         {
+            string dir = Directory.GetCurrentDirectory();
+            int ancho = this.Size.Width;
+            var seed = Environment.TickCount;
+            var random = new Random(seed);
+
+            for (int i = 0; i < 8; i++)
+            {
+                PictureBox pic = new PictureBox();
+                pic.Location = new Point(20*i+100 , 570);               
+                pic.Name = "pic" +i;
+                pic.Size = new Size(75, 75);
+                string edificio = "Edificio" + random.Next(1, 7).ToString() + ".png";
+                pic.ImageLocation = dir + @"\edificios\" + edificio;
+                pic.Image = new System.Drawing.Bitmap(Image.FromFile(dir + @"\edificios\" + edificio));
+                pic.SizeMode = PictureBoxSizeMode.AutoSize;
+                this.Controls.Add(pic);
+            }
 
         }
     }
