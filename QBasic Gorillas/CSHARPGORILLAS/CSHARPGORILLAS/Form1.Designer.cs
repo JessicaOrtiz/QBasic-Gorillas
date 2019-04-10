58fe2a891c70a01208ccc7d412cf5e1a9ada5a3b
Fernando Martinez <zackel.gaomon@gmail.com>
2019-04-10 15:55:10 -0500

    Correccion escenario y posicion de jugadores

--- QBasic Gorillas/CSHARPGORILLAS/CSHARPGORILLAS/Form1.Designer.cs
+++ QBasic Gorillas/CSHARPGORILLAS/CSHARPGORILLAS/Form1.Designer.cs
@@ -28,39 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // timer1
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(47, 394);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(28, 36);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(404, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 78);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(144, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Angulo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "peso";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(908, 452);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- QBasic Gorillas/CSHARPGORILLAS/CSHARPGORILLAS/Form1.cs
+++ QBasic Gorillas/CSHARPGORILLAS/CSHARPGORILLAS/Form1.cs
@@ -13,10 +13,21 @@ namespace CSHARPGORILLAS
 {
     public partial class Form1 : Form
     {
+        int tiempo = 0;
+        int jugador = 1;
+        List<Image> listEdificios = new List<Image>();
+        List<int> tamaños = new List<int>();
+        int tamImagen = 75;
+
         public Form1()
         {
             InitializeComponent();
             CreaEscenario();
+            label1.Text ="Turno jugador 1";
+            this.MaximizeBox =false;
+            timer1.Start();
+
+
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -27,23 +38,121 @@ namespace CSHARPGORILLAS
         private void CreaEscenario()
         {
             string dir = Directory.GetCurrentDirectory();
+            int posSol = 0;
+            int posSol2 = 0;
             int ancho = this.Size.Width;
             var seed = Environment.TickCount;
             var random = new Random(seed);
 
-            for (int i = 0; i < 8; i++)
+            while(posSol == posSol2)
             {
+                posSol = random.Next(1, (int)ancho / tamImagen);
+                posSol2 = random.Next(1, (int)ancho / tamImagen);
+            }
+
+           
+
+            for (int i = 0; i <(int) ancho/tamImagen; i++)
+            {
+               
                 PictureBox pic = new PictureBox();
-                pic.Location = new Point(20*i+100 , 570);               
-                pic.Name = "pic" +i;
-                pic.Size = new Size(75, 75);
                 string edificio = "Edificio" + random.Next(1, 7).ToString() + ".png";
+                Image img = Image.FromFile(dir + @"\edificios\" + edificio);
+                pic.Name = "pic";
+                int tam = random.Next(30, 200);
+                pic.Size = new Size(tamImagen, tamImagen + tam);
+                pic.Location = new Point(posicion(i), this.Height - (tamImagen+tam));
+                if (i == posSol)
+                {
+                    PictureBox picS1 = new PictureBox();
+                    Image imgS1;
+                    if (posSol > posSol2)
+                         imgS1= Image.FromFile(dir + @"\Personajes\soldado2.png");
+                    else
+                         imgS1 = Image.FromFile(dir + @"\Personajes\soldado.png");
+                    picS1.Name = "PicS1";
+                    picS1.Size = new Size(28, 36);
+                    picS1.Location = new Point(posicion(i)+30, this.Height - (tamImagen + tam)-36);
+                    picS1.Image = new System.Drawing.Bitmap(imgS1);
+                    picS1.SizeMode = PictureBoxSizeMode.StretchImage;
+                    this.Controls.Add(picS1);
+                }
+                if(i == posSol2)
+                {
+                    PictureBox picS2 = new PictureBox();
+                    Image imgS2;
+                    if (posSol < posSol2)
+                        imgS2 = Image.FromFile(dir + @"\Personajes\soldado2.png");
+                    else
+                        imgS2 = Image.FromFile(dir + @"\Personajes\soldado.png");
+                    picS2.Name = "PicS2";
+                    picS2.Size = new Size(28, 36);
+                    picS2.Location = new Point(posicion(i) + 37, this.Height - (tamImagen + tam) - 36);
+                    picS2.Image = new System.Drawing.Bitmap(imgS2);
+                    picS2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    this.Controls.Add(picS2);
+                }
+
+
                 pic.ImageLocation = dir + @"\edificios\" + edificio;
-                pic.Image = new System.Drawing.Bitmap(Image.FromFile(dir + @"\edificios\" + edificio));
-                pic.SizeMode = PictureBoxSizeMode.AutoSize;
+                pic.Image = new System.Drawing.Bitmap(img);
+                pic.SizeMode = PictureBoxSizeMode.StretchImage;
                 this.Controls.Add(pic);
             }
 
+            
+            int t = 0;
+
+
+        }
+
+        private int posicion(int ii)
+        {
+            int tam=0;
+
+            for(int i = 0; i < ii; i++)
+            {
+                tam += tamImagen;
+            }
+            return tam;
+        }
+
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tiempo++;
+            if (tiempo > 100)
+            {
+                timer1.Stop();
+                if (jugador == 1) 
+                    jugador =2;
+                else 
+                    jugador =1;
+                establece(jugador);
+                timer1.Start();
+
+            }
+        }
+
+        private void establece(int num)
+        {
+            label1.Text = "Turno del jugador" + num.ToString();
+            tiempo = 0;
+
+            if (num == 2)
+            {       
+                label2.Location = new Point(label2.Location.X + 500, label2.Location.Y);
+                textBox1.Location = new Point(textBox1.Location.X + 500, textBox1.Location.Y);
+                label3.Location = new Point(label3.Location.X + 500, label3.Location.Y);
+                textBox2.Location = new Point(textBox2.Location.X + 500, textBox2.Location.Y);
+            }
+            else
+            {             
+                label2.Location = new Point(label2.Location.X - 500, label2.Location.Y);
+                textBox1.Location = new Point(textBox1.Location.X -500, textBox1.Location.Y);
+                label3.Location = new Point(label3.Location.X - 500, label3.Location.Y);
+                textBox2.Location = new Point(textBox2.Location.X - 500, textBox2.Location.Y);
+            }
         }
     }
 }
